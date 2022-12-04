5fefe82e0b4353653955dbaefe857b2dbaff4d79
Youmna Samir <94459719+youmna3@users.noreply.github.com>
2022-12-04 12:42:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 // select multiple classes and loop throgh
+const close = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const open = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 const btnsOpenModel = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModel);
 for (let i = 0; i < btnsOpenModel.length; i++)
-  btnsOpenModel[i].addEventListener('click', () => {
-    console.log('b');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-btnCloseModel.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+  btnsOpenModel[i].addEventListener('click', open);
+
+btnCloseModel.addEventListener('click', close);
+overlay.addEventListener('click', close);
